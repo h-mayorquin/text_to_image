b4d00a94d8fb5508c234bbc703850056765a01ea
heberto <h.mayorquin@gmail.com>
2015-11-24 17:32:22 +0100

    Refractor ntlk-processing file, added ability so save letters as well

--- ntlk_corpus.py
+++ ntlk_corpus.py
@@ -4,15 +4,14 @@ from nltk.book import text7
 import h5py
 from aux_functions import downsample_letter
 
-word = text7[0]
-Nwords = 1000
+Nwords = len(text7)
 text = text7[0:Nwords]
 store_directory = './wall_street/'
 
 # Define the font
-font_source = './secrcode.ttf'
+font_source = './fonts/secrcode.ttf'
 # font_source = './Cocomat Ultralight-trial.ttf'
-font_source = './Chunk.ttf'
+font_source = './fonts/Chunk.ttf'
 
 # The 100 is the pixels, the 256 is the color, 1 is for only one pixel
 pixels = 20
@@ -24,26 +23,29 @@ font = ImageFont.truetype(font=font_source, size=size)
 
 # Parameters
 space = True
-save = False
+save_image = False
 to_list = True
 downsample = False
-renormalize = True
+renormalize = False
 number_format = np.int8
 number_format = np.float
 
 counter = 0
 image_list = []
+letters = []
 
 for word in text:
     # Go through the low caps version of the word
     for letter in word.lower():
         counter += 1
+        # Append letter to list\
+        letters.append(letter)
         # Create the image and draw the letter
         img = Image.new('1', (pixels, pixels), 256)
         d = ImageDraw.Draw(img)
         d.text((coord_x, coord_y), letter, font=font)
 
-        if save:
+        if save_image:
             img.save(store_directory + str(counter) + '.png')
 
         if to_list:
@@ -55,13 +57,15 @@ for word in text:
     # Add a space
     if space:
         counter += 1
+        # Append letter to list
+        letters.append(' ')
         # Create the image and draw the letter
         img = Image.new('1', (pixels, pixels), 256)
         d = ImageDraw.Draw(img)
         letter = ' '
         d.text((coord_x,coord_y), letter, font=font)
 
-        if save:
+        if save_image:
             img.save(store_directory + str(counter) + '.png')
 
         if to_list:
@@ -70,11 +74,16 @@ for word in text:
                 pix = downsample_letter(pix, 40, renormalize)
             image_list.append(pix)
 
-# Transform the list into a signal
+# Transform the list into an array
 signal = np.array(image_list)
+letters = np.asarray(letters)
+print('Data size is', signal.shape)
+print('Number of words is', Nwords)
 
 # Save this to a hdf5 data base using a context manager
 save_filename = './wall_street_data_small.hdf5'
-with h5py.File(save_filename, 'r') as f:
-    f.create_dataset('signal', data=signal, dtype=number_format)
+with h5py.File(save_filename, 'w') as f:
+    f['/signal'] = signal
 
+# Save the letters
+np.save('./wall_street_letters.npy', letters)
